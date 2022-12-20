55add696e4097d318c77c5a05cbe1ed1aff16eb4
Anna Volfman <aniya_07@ukr.net>
2022-12-20 19:04:47 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const imagesMarkup = createGallery(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', imagesMarkup);
+
+galleryEl.addEventListener('click', onGalleryClick)
+
+function createGallery(images) {
+    return images.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}" rel="noopener noreferrer nofollow">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>`
+    }).join(' ');
+}
+
+function onGalleryClick(event) {
+
+    const isGalleryImg = event.target.classList.contains('gallery__image');
+
+        if (!isGalleryImg) {
+            return;
+        }
+    
+    const imgUrl = event.target.dataset.source;
+}
+
+
